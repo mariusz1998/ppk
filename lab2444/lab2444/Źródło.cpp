c71fac0281737174b848bc36a6275b408e69d4d4
Mariusz Drynda <mariusz1998@wp.pl>
2017-12-03 16:15:35 +0100

    hello

---
+++ lab2444/lab2444/Źródło.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <time.h>
+#define SIZE1 10
+#define SIZE 12
+using namespace std;
+int main()
+{
+	srand(time(NULL));
+	int tab[SIZE1][SIZE];
+	for (int i = 0; i < SIZE1; i++)
+	{
+		for (int j = 0; i < SIZE; j++)
+		{
+			tab[i][j] = 12;
+		}
+	}
+	for (int i = 0; i < SIZE1; i++)
+	{
+		for (int j = 0; i < SIZE; j++)
+
+			cout << tab[i][j] << endl;
+	}
+	system("pause");
+	return 0;
+
+}
